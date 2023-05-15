766e77980654d8f84c4b9d8ed9b72dd795d5ae44
Siddharth Kothari <sidkothari005@gmail.com>
2023-05-15 19:22:13 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { services } from '../constants'
+import {fadeIn, textVariant} from '../utils/motion'
+import { sectionWrapper } from '../hoc'
+
+const ServiceCard = ({index, title, icon}) => {
+  return(
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right","spring",0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div 
+          options={{
+              max: 45,
+              scale: 1,
+              speed: 450
+            }} 
+          
+          className= "bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center"
+          >
+            <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+            <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={ styles.sectionSubText }>Introduction</p>
+        <h2 className={ styles.sectionHeadText }>Overview</h2>
+        <motion.p 
+          variants={fadeIn("","",0.1,1)}
+          className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          I am a skilled Software Developer with over a year of experience in PHP, HTML, JavaScript, CSS, ReactJs, REST APIs and MySQL tools. Creating website layouts and user interfaces using standard HTML/CSS practices is one of my core strengths. I believe that effective collaboration and communication are critical to delivering the best possible solutions. I also have experience integrating data from various back-end services and databases.
+        </motion.p>
+
+        <div className='mt-20 flex flex-wrap gap-10'>
+          {services.map((service,index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+        </div>
+
+        
+      </motion.div>
+    </>
+  )
+}
+
+export default sectionWrapper(About, "about")
---
+++ src/constants/index.js
@@ -0,0 +1,241 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+} from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "resume",
+        title: "Resume",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "Frontend Developer",
+        icon: backend,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "GitHub",
+        icon: git,
+    },
+    {
+        name: "PHP",
+        icon: git,
+    },
+    {
+        name: "BootStrap",
+        icon: tailwind,
+    },
+
+    {
+        name: "WordPress",
+        icon: tailwind,
+    },
+    {
+        name: "MySQL",
+        icon: tailwind,
+    },
+];
+
+const experiences = [
+    {
+        title: "Software Developer",
+        company_name: "i2e Consulting",
+        icon: starbucks,
+        iconBg: "#383E56",
+        date: "Aug 2022 - Ongoing",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Student Trainee",
+        company_name: "i2e Consulting",
+        icon: tesla,
+        iconBg: "#E6DEDD",
+        date: "Mar 2022 - Aug 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "UI/UX & Functionality Tester Intern",
+        company_name: "DigiLocker",
+        icon: shopify,
+        iconBg: "#383E56",
+        date: "Jan 2021 - May 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+const testimonials = [
+    // {
+    //     testimonial:
+    //         "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    //     name: "Sara Lee",
+    //     designation: "CFO",
+    //     company: "Acme Co",
+    //     image: "https://randomuser.me/api/portraits/women/4.jpg",
+    // },
+    // {
+    //     testimonial:
+    //         "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    //     name: "Chris Brown",
+    //     designation: "COO",
+    //     company: "DEF Corp",
+    //     image: "https://randomuser.me/api/portraits/men/5.jpg",
+    // },
+    // {
+    //     testimonial:
+    //         "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    //     name: "Lisa Wang",
+    //     designation: "CTO",
+    //     company: "456 Enterprises",
+    //     image: "https://randomuser.me/api/portraits/women/6.jpg",
+    // },
+];
+
+const projects = [
+    {
+        name: "AI Based Game Development ",
+        description:
+            "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        tags: [
+            {
+                name: "C++",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: carrent,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Digital Video Game Store ",
+        description:
+            "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        tags: [
+            {
+                name: "PHP",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "JavaScript",
+                color: "green-text-gradient",
+            },
+            {
+                name: "HTML",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: jobit,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Portfolio Website",
+        description:
+            "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        tags: [
+            {
+                name: "nextjs",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "reactjs",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwindcss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: tripguide,
+        source_code_link: "https://github.com/",
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects };
