51b12838e3edb4a1b74254ddbe590b7985c46398
Siddharth Kothari <sidkothari005@gmail.com>
2023-06-17 17:27:03 +0530

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+  return (
+    <div className="border-t border-white text-white-100 text-center py-8 grid gap-2">
+        <p>Developed by Siddharth Kothari</p>
+        <p>Design and inspiration by JavaScript Mastery</p>
+        
+    </div>
+  )
+}
+
+export default Footer
