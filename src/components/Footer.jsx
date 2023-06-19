3fafb49c1c5c66ea288c267409fd9b333dd56875
Siddharth Kothari <sidkothari005@gmail.com>
2023-06-19 11:08:52 +0530

    footer credit change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="border-t border-white text-white-100 text-center py-8 grid gap-2">
         <p>Developed by Siddharth Kothari</p>
-        <p>Design and inspiration by JavaScript Mastery</p>
+        <p>Design and inspired by JavaScript Mastery</p>
         
     </div>
   )
