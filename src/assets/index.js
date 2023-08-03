82ec688caadee883d6864665a52f487e09a12dd2
Siddharth Kothari <sidkothari005@gmail.com>
2023-08-03 13:43:13 +0530

    hero component changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import avatar from "./avatar.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -67,4 +68,5 @@ mysql,
   polaris,
 portfolio,
   tripguide,
+  avatar,
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 
-import {motion} from 'framer-motion';
-
+// import {motion} from 'framer-motion';
+import { avatar } from '../assets';
 
 import {styles} from '../styles';
 //import { ComputersCanvas } from './canvas';
@@ -8,10 +8,12 @@ import {styles} from '../styles';
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} grid h-screen text-center place-content-center max-w-7xl mx-auto -mt-12`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <img
+            src={avatar}
+            className='w-[250px]'
+          />
         </div>
         <div className=''>
           <h1 className={`${styles.heroHeadText}`}>Hi, I&apos;m <span className="text-[#915eff]"> Siddharth</span></h1>
@@ -25,7 +27,7 @@ const Hero = () => {
       </div>
         {/* <ComputersCanvas /> */}
 
-        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href='#about'>
             <div className='w-[35px] h-[64px] rounded-3xl border-secondary border-4 flex justify-center items-start p-2'>
               <motion.dev 
@@ -41,7 +43,7 @@ const Hero = () => {
               /> 
             </div>
           </a>
-        </div>
+        </div> */}
     </section>
   )
 }
