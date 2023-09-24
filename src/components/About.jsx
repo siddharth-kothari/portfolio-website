50aa17005ff5ad0e013eeb5bfbedb3f831e82b39
Siddharth Kothari <sidkothari005@gmail.com>
2023-09-24 18:26:22 +0530

    Minor changes to footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,33 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/no-unknown-property */
 
 import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
 import { services } from '../constants'
-import {fadeIn, textVariant} from '../utils/motion'
+import { fadeIn, textVariant } from '../utils/motion'
 import { sectionWrapper } from '../hoc'
 
-const ServiceCard = ({index, title, icon}) => {
-  return(
+const ServiceCard = ({ index, title, icon }) => {
+  return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn("right","spring",0.5 * index, 0.75)}
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
-        <div 
+        <div
           options={{
-              max: 45,
-              scale: 1,
-              speed: 450
-            }} 
-          
-          className= "bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center"
-          >
-            <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
-            <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center"
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
         </div>
       </motion.div>
     </Tilt>
@@ -36,22 +39,22 @@ const About = () => {
   return (
     <div className='text-center'>
       <motion.div variants={textVariant()} >
-        <p className={ styles.sectionSubText }>Introduction</p>
-        <h2 className={ styles.sectionHeadText }>Overview</h2>
-        <motion.p 
-          variants={fadeIn("","",0.1,1)}
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
           className='mt-4 text-secondary text-[17px] text-center max-w-3xl leading-[30px] m-auto'
         >
           I am a skilled Software Developer with over a year of experience in PHP, HTML, JavaScript, CSS, ReactJs, REST APIs and MySQL tools. Creating website layouts and user interfaces using standard HTML/CSS practices is one of my core strengths. I believe that effective collaboration and communication are critical to delivering the best possible solutions. I also have experience integrating data from various back-end services and databases.
         </motion.p>
 
         <div className='mt-10 flex justify-center flex-wrap gap-10'>
-          {services.map((service,index) => (
+          {services.map((service, index) => (
             <ServiceCard key={service.title} index={index} {...service} />
           ))}
         </div>
 
-        
+
       </motion.div>
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,77 @@
 
 
+
+
+
+import { styles } from "./../styles";
+import { motion } from "framer-motion";
+import { zoomIn } from "./../utils/motion";
+
 const Footer = () => {
   return (
-    <div className="border-t border-white text-white-100 text-center py-8 grid gap-2">
-        <p>Developed by Siddharth Kothari</p>
-        <p>Design and inspired by JavaScript Mastery</p>
-        
-    </div>
-  )
-}
+    <footer
+      className={`${styles.paddingX} w-full flex items-center py-10 z-20 bg-primary`}
+    >
+      <div className="w-full block text-center md:flex justify-between items-center max-w-7xl mx-auto">
+        <p className="font-manrope mb-5 md:m-0">
+          Developed by Siddharth Kothari
+        </p>
 
-export default Footer
+        <ul className="list-none flex flex-row gap-10 justify-center">
+          <li className={`cursor-pointer`}>
+            <a href="https://www.instagram.com/glam2door/" target="blank">
+              <motion.p
+                variants={zoomIn(0, 0.2)}
+                whileHover="show"
+                initial="hidden"
+              >
+
+              </motion.p>
+            </a>
+          </li>
+
+          <li className={`cursor-pointer`}>
+            <a
+              href=""
+              target="blank"
+            >
+              <motion.p
+                variants={zoomIn(0, 0.2)}
+                whileHover="show"
+                initial="hidden"
+              >
+
+              </motion.p>
+            </a>
+          </li>
+
+          <li className={`cursor-pointer`}>
+            <a href="mailto:support@glam2door.com" target="blank">
+              <motion.p
+                variants={zoomIn(0, 0.2)}
+                whileHover="show"
+                initial="hidden"
+              >
+
+              </motion.p>
+            </a>
+          </li>
+
+          <li className={`cursor-pointer`}>
+            <a href="tel:+919975136016" target="blank">
+              <motion.p
+                variants={zoomIn(0, 0.2)}
+                whileHover="show"
+                initial="hidden"
+              >
+
+              </motion.p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
